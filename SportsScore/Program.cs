33629ef3a2b000d1c1c942acf0e0fefd7390c31b
Chris Marais <cmarais@caci.co.uk>
2024-01-05 16:19:21 +0000

    mid dev test

---
+++ SportsScore/Program.cs
@@ -0,0 +1,64 @@
+﻿string score = "1001010101111011101111";
+int n = 15;
+Sport.predictWinner(score, n);
+
+internal class Sport
+{
+    // function for winner prediction
+    public static void predictWinner(string score, int n)
+    {
+        int[] count = new int[2];
+        int i;
+        for (i = 0; i < score.Length; i++)
+        {
+            // increase count
+            count[score[i] - '0']++;
+
+            // check losing
+            // condition
+            if (count[0] == n && count[1] < n - 1)
+            {
+                Console.Write("Team 1 lost");
+                return;
+            }
+
+            // check winning condition
+            if (count[1] == n && count[0] < n - 1)
+            {
+                Console.Write("Team 1 won");
+                return;
+            }
+
+            if (count[0] == n - 1 && count[1] == n - 1)
+            {
+                count[0] = 0;
+                count[1] = 0;
+                break;
+            }
+        }
+
+        for (i++; i < score.Length; i++)
+        {
+            // increase count
+            count[score[i] - '0']++;
+
+            // check for 2 point lead
+            if (Math.Abs(count[0] - count[1]) == 2)
+            {
+                // condition of lost
+                if (count[0] > count[1])
+                {
+                    Console.Write("Team 1 lost");
+                }
+
+                // condition of win
+                else
+                {
+                    Console.Write("Team 1 won");
+                }
+
+                return;
+            }
+        }
+    }
+}
